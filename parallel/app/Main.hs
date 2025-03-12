521a37bf43dc844c4dbd3c86cbb03b64193551a7
mhm2002 <mhm2002@hw-em250-lx68.hw.ac.uk>
2025-03-12 18:29:08 +0000

    changing machines

---
+++ parallel/app/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Criterion.Measurement
+import GA (initializePopulation, gaIterate)
+
+main :: IO ()
+main = do
+  start <- getTime
+  population <- initializePopulation 5000 100
+  fitness <- gaIterate 0 population 200 2 2.0
+  putStrLn $ "Final fitness: " ++ show fitness
+  end <- getTime
+  let timeTaken = end - start
+  putStrLn $ "Time Taken: " ++ show timeTaken
--- src/Benchmark.hs
+++
@@ -1,4 +0,0 @@
-module Benchmark (sphere) where
-
-sphere :: [Double] -> Double
-sphere individual = sum $ map (\x -> x ^ 2) individual
