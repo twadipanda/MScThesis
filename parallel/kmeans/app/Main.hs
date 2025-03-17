ae4375d9d798ea7ce05a95ee68a7ef4533eabe8a
mhm2002 <mhm2002@hw-em250-lx68.hw.ac.uk>
2025-03-17 11:26:56 +0000

    chore: small update to remove prints

--- parallel/kmeans/app/Main.hs
+++ parallel/kmeans/app/Main.hs
@@ -2,26 +2,18 @@ module Main where
 
 import Criterion.Measurement
 import Control.DeepSeq
+import Control.Exception (evaluate)
 import Read
 import Kmeans
-import DistanceMetrics
 -- import qualified Data.Vector.Unboxed as U
 
 main :: IO ()
 main = do
-    --putStrLn "Reading Data..."
-    points <- reada "letter.txt"
-    let parsed = parseInput points
-    deepseq parsed print ""
-    let centers = getCenters 26 parsed
-   -- putStrLn "Starting K-means with k = 3 on dim16 data..."
+    points <- reada "worms_64d.txt"
+    parsed <- evaluate $ force $ parseInput points
+    centers <- evaluate $ force $ getCenters 25 parsed
     start <- getTime
-    let newMeans = kmeansIter 26 parsed centers
-    deepseq newMeans print ""
-    -- let temp = euclideanDistance1 (U.fromList (parsed!!0)) (U.fromList (parsed!!1))
-    -- deepseq temp print ""
+    _ <- evaluate $ force $ kmeansIter 25 parsed centers
     end <- getTime
     let timeTaken = end - start
-    -- let newMeans = kmeans 3 0 parsed centers
     print timeTaken
-    print newMeans
